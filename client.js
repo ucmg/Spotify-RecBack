a3bca3d0681ce0ffdd20fda6e5e0a803c8e62d48
Glitch (nelson) <none>
2018-02-05 14:47:24 +0000

    🌘🗻 Checkpoint client.js:292132/1

--- client.js
+++ client.js
@@ -18,7 +18,7 @@ const authEndpoint = 'https://accounts.spotify.com/authorize';
 
 // Replace with your app's client ID, redirect URI and desired scopes
 const clientId = '593219e3509a40e499f266c2c4fd6f5c';
-const redirectUri = 'https://nelson2.glitch.me/';
+const redirectUri = 'https://nelson.glitch.me/';
 const scopes = [
   'streaming',
   'user-read-birthdate',
